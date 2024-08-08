31c772d752c706a82f93eb6a5ee65b0958fa122d
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2024-08-08 17:19:34 -0300

    projeto-08

---
+++ projeto-08/main.js
@@ -0,0 +1,39 @@
+const lampDesligada = document.getElementById("lampDesligada")
+const lampLigada = document.getElementById("lampLigada")
+const lampQuebrada = document.getElementById("lampQuebrada")
+const divImgs = document.getElementById("imgs")
+const btnLigar = document.getElementById("ligar")
+const btnDesligar = document.getElementById("desligar")
+
+function ligarLamp() {
+    lampDesligada.classList.add("hiden")
+    lampLigada.classList.remove("hiden")
+    lampQuebrada.classList.add("hiden")
+}
+
+function desligarLamp() {
+    lampDesligada.classList.remove("hiden")
+    lampLigada.classList.add("hiden")
+    lampQuebrada.classList.add("hiden")
+}
+
+function quebrarLamp() {
+    lampDesligada.classList.add("hiden")
+    lampLigada.classList.add("hiden")
+    lampQuebrada.classList.remove("hiden")
+}
+
+btnLigar.addEventListener('click', ligarLamp)
+
+btnDesligar.addEventListener('click', desligarLamp)
+
+divImgs.addEventListener('dblclick', () => {
+    quebrarLamp()
+
+    btnDesligar.removeEventListener('click', desligarLamp)
+    btnLigar.removeEventListener('click', ligarLamp)
+})
+
+// divImgs.addEventListener("mouseleave", desligarLamp)
+// divImgs.addEventListener("mouseover", ligarLamp)
+
