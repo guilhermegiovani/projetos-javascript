699779963f4f5bac07308e5e1d72596cd8601471
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2024-08-10 17:57:10 -0300

    projeto-09 correção

--- projeto-09/main.js
+++ projeto-09/main.js
@@ -3,35 +3,49 @@ const vermelho = document.getElementById("vermelho")
 const amarelo = document.getElementById("amarelo")
 const verde = document.getElementById("verde")
 const automatico = document.getElementById("auto")
+let intervalId = null
+let timeOutRedId = null
+let timeOutYellowId = null
+let timeOutGreenId = null
 
 function luzVermelho() {
+    pararAutomático()
     semaro.src = "./img/vermelho.png"
 }
 
 function luzAmarelo() {
+    pararAutomático()
     semaro.src = "./img/amarelo.png"
 }
 
 function luzVerde() {
+    pararAutomático()
     semaro.src = "./img/verde.png"
 }
 
 function semaforoAuto() {
-    setInterval(() => {
+    intervalId = setInterval(() => {
 
-        setTimeout(() => {
+        timeOutRedId = setTimeout(() => {
             semaro.src = "./img/vermelho.png"
         }, 500)
 
-        setTimeout(() => {
+        timeOutYellowId = setTimeout(() => {
             semaro.src = "./img/amarelo.png"
         }, 1000)
 
-        setTimeout(() => {
+        timeOutGreenId = setTimeout(() => {
             semaro.src = "./img/verde.png"
         }, 1500)
         
     }, 1700)
+}
+
+function pararAutomático() {
+    clearTimeout(timeOutRedId)
+    clearTimeout(timeOutYellowId)
+    clearTimeout(timeOutGreenId)
+    clearInterval(intervalId)
 }
 
 vermelho.addEventListener('click', luzVermelho)
