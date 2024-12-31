acfc9a56ca54de5a964434e8ca3e0954fa605b18
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2024-12-31 17:46:23 -0300

    projeto-06
    finalizado

---
+++ projeto-06/main.js
@@ -0,0 +1,33 @@
+// verificação de email
+
+function emailValido(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return emailRegex.test(email)
+}
+
+const emailForm = document.getElementById("emailForm")
+
+emailForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const emailInput = document.getElementById("emailInput").value
+    const messageElement = document.getElementById("mensagem")
+
+    if(emailValido(emailInput)) {
+        
+        messageElement.textContent = "E-mail válido!"
+        messageElement.className = "success"
+
+        setTimeout(() => {
+            messageElement.classList.toggle("hidden")
+        }, 3000)
+    } else {
+        messageElement.textContent = "Please provide a valid email address"
+        messageElement.className = "error"
+
+        setTimeout(() => {
+            messageElement.classList.toggle("hidden")
+        }, 3000)
+    }
+})
