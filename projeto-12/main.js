e8c65c6b6ee51cdd449bbc07d16ce7e8b2d727b2
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-01-27 17:56:50 -0300

    projeto-12

--- projeto-12/main.js
+++ projeto-12/main.js
@@ -7,38 +7,75 @@ function verificarEmail(email) {
     return emailRegex.test(email)
 }
 
-// function fotoAvatar() {
-//     const avatar_foto = document.getElementById("upload_avatar").value
+const upload_avatar = document.querySelector(".upload_avatar")
 
-//     // https://www.youtube.com/watch?v=Gc5dhlbmdLI - como mostrar img no input
+function onEnter(event) {
+    event.preventDefault()
+    upload_avatar.classList.add("active")
+}
 
-//     if(avatar_foto.files.length != 0) {
-//         const img_avatar = document.getElementById("foto_avatar")
+function onLeave() {
+    upload_avatar.classList.remove("active")
+}
 
-//         img_avatar.src = "assets/images/image-avatar.jpg"
-//     }
-// }
+upload_avatar.addEventListener("dragover", onEnter)
+upload_avatar.addEventListener("dragleave", onLeave)
+upload_avatar.addEventListener("dragend", onLeave)
+upload_avatar.addEventListener("drop", (event) => {
+    event.preventDefault()
+    
+    onLeave()
+})
 
-// fotoAvatar()
+const avatar_foto = document.getElementById("upload_avatar")
+
+avatar_foto.addEventListener('change', (event) => {
+    event.preventDefault()
+
+    if(avatar_foto.files.length > 0) {
+        console.log(avatar_foto.files)
+
+        const maxSize = 500 * 1024
+        const type = avatar_foto.files[0].type
+        const formats = ["image/jpeg", "image/jpg", "image/png"]
+
+        if(!formats.includes(type)) {
+            alert("Formato do arquivo não é permitido! Tente Novamente!")
+            return
+        }
+
+        if(avatar_foto.files[0].size > maxSize) {
+            alert("Tamanho do arquivo ultrapassou o limite! Tente Novamente")
+            return
+        }
+
+        const img_avatar = document.getElementById("foto_avatar")
+        img_avatar.src = `assets/images/${avatar_foto.files[0].name}`
+
+        img_avatar.style.borderRadius = "10px"
+        img_avatar.style.width = "100%"
+        document.querySelector("#span_avatar").style.padding = "0px"
+    }
+    
+})
 
 function submitForms() {
-    // const avatar_foto = document.getElementById("upload_avatar").value
     const full_name = document.getElementById("full_name").value
     const email_address = document.getElementById("email_address").value
     const github_username = document.getElementById("github_username").value
-    return [avatar_foto, full_name, email_address, github_username]
+    return [full_name, email_address, github_username]
 }
 
-function ticket() {
-    const [avatar_foto, full_name, email_address, github_username] = submitForms()
+// function ticket() {
+//     const [avatar_foto, full_name, email_address, github_username] = submitForms()
 
-    console.log(full_name)
-    console.log(email_address)
-    console.log(github_username)
-}
+//     console.log(full_name)
+//     console.log(email_address)
+//     console.log(github_username)
+// }
 
-forms_conf.addEventListener('submit', (event) => {
-    event.preventDefault()
+// forms_conf.addEventListener('submit', (event) => {
+//     event.preventDefault()
 
-    ticket()
-})
+//     ticket()
+// })
