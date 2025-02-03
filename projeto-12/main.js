d095e655ef010fca6469ea006c9cbdaac9ddf791
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-02-03 18:05:57 -0300

    Projeto-12

--- projeto-12/main.js
+++ projeto-12/main.js
@@ -92,7 +92,28 @@ avatar_foto.addEventListener('change', (event) => {
 })
 
 function submitForms() {
+    const svg_icon = criarInfoIconSvg()
+
     const full_name = document.getElementById("full_name").value
+
+    if(full_name == "") {
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        const container_fullname = document.querySelector(".container_fullname")
+        const input_fullname = document.getElementById("full_name")
+
+        div.classList.toggle("error_mensage")
+        div.classList.toggle("error")
+
+        input_fullname.classList.toggle("input_error")
+
+        p.textContent = "Please a enter a valid Full Name."
+        p.className = "text_error"
+        svg_icon.classList.add("img_icon_email")
+
+        div.append(svg_icon, p)
+        container_fullname.appendChild(div)
+    }
 
     const email_address = document.getElementById("email_address").value
 
@@ -100,64 +121,97 @@ function submitForms() {
         const div = document.createElement("div")
         const p = document.createElement("p")
         const container_email = document.querySelector(".container_email")
+        const input_email = document.getElementById("email_address")
 
-        const svgNS = "http://www.w3.org/2000/svg"
-        const svg_icon = document.createElementNS(svgNS, "svg")
-
-        svg_icon.setAttribute("width", "16")
-        svg_icon.setAttribute("height", "16")
-        svg_icon.setAttribute("viewBox", "0 0 16 16")
-        svg_icon.setAttribute("fill", "none")
-
-        const path1 = document.createElementNS(svgNS, "path")
-
-        path1.setAttribute("stroke", "#D1D0D5")
-        path1.setAttribute("stroke-linecap", "round")
-        path1.setAttribute("stroke-linejoin", "round")
-        path1.setAttribute("d", "M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z")
-        svg_icon.appendChild(path1)
-
-        const path2 = document.createElementNS(svgNS, "path")
-
-        path2.setAttribute("fill", "#D1D0D5")
-        path2.setAttribute("d", "M8.004 10.462V7.596ZM8 5.57v-.042Z")
-        svg_icon.appendChild(path2)
-
-        const path3 = document.createElementNS(svgNS, "path")
-
-        path3.setAttribute("stroke", "#D1D0D5")
-        path3.setAttribute("stroke-linecap", "round")
-        path3.setAttribute("stroke-linejoin", "round")
-        path3.setAttribute("d", "M8.004 10.462V7.596M8 5.569v-.042")
-        svg_icon.appendChild(path3)
-
-        div.className = "error_email"
+        div.classList.toggle("error_mensage")
         div.classList.toggle("error")
 
-        p.textContent = "Please a enter a valid email address."
-        p.className = "text_error_email"
+        input_email.classList.toggle("input_error")
 
-        // img_icon.src = "assets/images/icon-info.svg"
-        svg_icon.classList.add("img_icon_email")
+        svg_icon.classList.add("img_icon")
+        p.textContent = "Please a enter a valid email address."
+        p.className = "text_error"
         
-        container_email.appendChild(div)
         div.append(svg_icon, p)
+        container_email.appendChild(div)
 
         // setTimeout(() => {
         //     container_email.removeChild(div)
+        //     input_email.classList.toggle("input_error")
         // }, 3000)
     }
 
     const github_username = document.getElementById("github_username").value
 
+    if(github_username == "") {
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        const container_github = document.querySelector(".container_github")
+        const input_github = document.getElementById("github_username")
+
+        div.classList.toggle("error_mensage")
+        div.classList.toggle("error")
+
+        input_github.classList.toggle("input_error")
+
+        svg_icon.classList.add("img_icon")
+        p.textContent = "Please a enter a valid github username."
+        p.className = "text_error"
+        
+        div.append(svg_icon, p)
+        container_github.appendChild(div)
+    }
+
+    // if() {}
+
     return [full_name, email_address, github_username]
+}
+
+function criarInfoIconSvg() {
+    const svgNS = "http://www.w3.org/2000/svg"
+    const svg_icon = document.createElementNS(svgNS, "svg")
+
+    svg_icon.setAttribute("width", "16")
+    svg_icon.setAttribute("height", "16")
+    svg_icon.setAttribute("viewBox", "0 0 16 16")
+    svg_icon.setAttribute("fill", "none")
+
+    const path1 = document.createElementNS(svgNS, "path")
+
+    path1.setAttribute("stroke", "#D1D0D5")
+    path1.setAttribute("stroke-linecap", "round")
+    path1.setAttribute("stroke-linejoin", "round")
+    path1.setAttribute("d", "M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z")
+    svg_icon.appendChild(path1)
+
+    const path2 = document.createElementNS(svgNS, "path")
+
+    path2.setAttribute("fill", "#D1D0D5")
+    path2.setAttribute("d", "M8.004 10.462V7.596ZM8 5.57v-.042Z")
+    svg_icon.appendChild(path2)
+
+    const path3 = document.createElementNS(svgNS, "path")
+
+    path3.setAttribute("stroke", "#D1D0D5")
+    path3.setAttribute("stroke-linecap", "round")
+    path3.setAttribute("stroke-linejoin", "round")
+    path3.setAttribute("d", "M8.004 10.462V7.596M8 5.569v-.042")
+    svg_icon.appendChild(path3)
+
+    return svg_icon
 }
 
 function ticket() {
     const [full_name, email_address, github_username] = submitForms()
 
     const ticket_avatar = document.querySelector(".ticket_avatar")
-    ticket_avatar.src = `assets/images/${avatar_foto.files[0].name}`
+
+    if(!avatar_foto.files.length > 0) {
+        alert("Adicione um arquivo!")
+    } else {
+        ticket_avatar.src = `assets/images/${avatar_foto.files[0].name}`
+    }
+
 
     const span_name = document.querySelector(".span_name")
     const name_ticket = document.querySelector(".name")
